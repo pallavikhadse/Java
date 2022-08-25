3750e1c8d0bbc98e470232e1ff6b0471f81b89d1
pallavikhadse <khadse09pallavi@gmail.com>
2022-08-26 01:47:36 +0200

    ReverseArray_Example

---
+++ ReverseArray.java
@@ -0,0 +1,15 @@
+public class ReverseArray {
+    public static void main(String []args){
+        int [] array = {1, 2, 3, 4 , 5, 6, 7};
+
+        for(int i=0; i<array.length/2; i++){
+            int temp = array[i];
+            array[i] = array[array.length-i-1];
+            array[array.length-i-1] = temp;
+
+            System.out.println(array[i]);
+        }       
+        
+    }
+    
+}
